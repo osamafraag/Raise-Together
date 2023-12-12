6b94d9862d1d9063e050e6985b7cf3bd4bfd2035
osamafraag <osamafraag19@gmail.com>
2023-12-12 19:38:07 +0200

    deployment

--- RaiseTogether/settings.py
+++ RaiseTogether/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-bls*k(y9s%4^kbjyukftyxx3)*@odqw0$n82hdzg5ix4skv+h6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
